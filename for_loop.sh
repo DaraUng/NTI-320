d73d0b0baafcebd90b1ab5c12739106891f4339c
Dara <43659748+DaraUng@users.noreply.github.com>
2019-05-02 20:03:37 -0700

    Update for_loop.sh

--- for_loop.sh
+++ for_loop.sh
@@ -4,8 +4,10 @@ for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d"
     echo $servername;
     serverip=$( gcloud compute instances list | grep $servername | awk '{print $4}');
     echo $serverip ;
+    bash scp_nagios web-a 10.150.0.5
 done
 
+gcloud compute ssh dara_ung@nagios-install --command='sudo systemctl resstart nagios'
 #awk is the last name initial for the writer.
 #awk is for colum
 #sed is for roll
