cf82f98c26ab97f3ca1ab50cd0320d44ce1e9f32
Dara <43659748+DaraUng@users.noreply.github.com>
2019-05-02 19:58:14 -0700

    Update for_loop.sh

--- for_loop.sh
+++ for_loop.sh
@@ -1,6 +1,6 @@
     
 #!/bin/bash
-for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" );  do 
+for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios-install );  do 
     echo $servername;
     serverip=$( gcloud compute instances list | grep $servername | awk '{print $4}');
     echo $serverip ;
@@ -9,4 +9,5 @@ done
 #awk is the last name initial for the writer.
 #awk is for colum
 #sed is for roll
-#
+#sed is search and replace
+#grep is remove
