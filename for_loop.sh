0bc19e1983ea9483424a839dfd95c6a70afb1818
Dara <43659748+DaraUng@users.noreply.github.com>
2019-05-02 20:41:25 -0700

    Update for_loop.sh

--- for_loop.sh
+++ for_loop.sh
@@ -13,3 +13,16 @@ gcloud compute ssh --zone us-east1-b dara_ung@nagios-install --command='sudo sys
 #sed is for roll
 #sed is search and replace
 #grep is remove
+
+for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" );  do 
+    echo $servername;
+    serverip=$( gcloud compute instances list | grep $servername | awk '{print $0}');
+    echo $serverip ;
+done
+
+for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios-install )
+for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios-install );  do gcloud compute ssh --zone us-east1-b dara_ung@nagios-install --command='sudo systemctl resstart nagios'
+
+
+
+for servername in $( gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios-a );  do 
