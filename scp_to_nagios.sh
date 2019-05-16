18d25cff3449f9aaa1b22b8e70cdd41d18433e24
Dara <43659748+DaraUng@users.noreply.github.com>
2019-05-15 20:34:20 -0700

    Update scp_to_nagios.sh

--- scp_to_nagios.sh
+++ scp_to_nagios.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 bash generate_config.sh $1 $2
 gcloud compute scp --zone us-west1-b $1.cfg dara_ung@nagios-install:/etc/nagios/servers
-gcloud compute ssh --zone us-west1-b dara_ung@nagios-install--command='sudo /usr/sbin/nagios -v /etc/nagios/nagios.cfg'
+gcloud compute ssh --zone us-west1-b dara_ung@nagios-install --command='sudo /usr/sbin/nagios -v /etc/nagios/nagios.cfg'
 
 #chmod is the permeission read write execute.
 #dara_ung@nagios-install meaning that login as dara_ung and go to this server (nagios-install)
