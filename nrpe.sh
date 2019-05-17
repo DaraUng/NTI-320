915b03858e1a436f82acaf49a015eddc70dcd35e
Dara <43659748+DaraUng@users.noreply.github.com>
2019-05-16 20:35:24 -0700

    Update nrpe.sh

--- nrpe.sh
+++ nrpe.sh
@@ -14,18 +14,26 @@ systemctl start nrpe
 sed -i 's/allowed_hosts=127.0.0.1/allowed_hosts=127.0.0.1, 10.138.15.217/g' /etc/nagios/nrpe.cfg
 sed -i "s,command[check_hda1]=/usr/lib64/nagios/plugins/check_disk -w 20% -c 10% -p /dev/hda1,command[check_disk]=/usr/lib64/nagios/plugins/check_disk -w 20% -c 10% -p /dev/sda1,g" /etc/nagios/nrpe.cfg
 
-sed -i 's,#command\[check_users\]=/usr/lib64/nagios/plugins/check_users \$ARG1\$,commannd\[check_users\]=/usr/lib64/nagios/plugins/check_users -w 5 -c 10,g' /etc/nagios/nrpe.cfg
-sed -i 's,#command\[check_users\]=/usr/lib64/nagios/plugins/check_users \$ARG1\$,commannd\[check_users\]=/usr/lib64/nagios/plugins/check_users -w 5 -c 10,g' /etc/nagios/nrpe.cfg
+sed -i 's|#command\[check_users\]=/usr/lib64/nagios/plugins/check_users \$ARG1\$,commannd\[check_users\]=/usr/lib64/nagios/plugins/check_users -w 5 -c 10|g' /etc/nagios/nrpe.cfg
+sed -i 's|#command\[check_users\]=/usr/lib64/nagios/plugins/check_users \$ARG1\$,commannd\[check_users\]=/usr/lib64/nagios/plugins/check_users -w 5 -c 10|g' /etc/nagios/nrpe.cfg
 
 
 
 
+sed -i 's|#command\[check_load\]=/usr/lib64/nagios/plugins/check_load \$ARG1\$|commannd\[check_load\]=/usr/lib64/nagios/plugins/check_load -w 15,10,5 -c 30,25,20|g' /etc/nagios/nrpe.cfg
+#sed -i 's|#command\[check_load\]=/usr/lib64/nagios/plugins/check_load \$ARG1\$|commannd\[check_load\]=/usr/lib64/nagios/plugins/check_load -w 15,10,5 -c 30,25,20|g' /etc/nagios/nrpe.cfg
 
-sed -i 's,#command\[check_load\]=/usr/lib64/nagios/plugins/check_load \$ARG1\$,commannd\[check_load\]=/usr/lib64/nagios/plugins/check_load -w 15,10,5 -c 30,25,20,g' /etc/nagios/nrpe.cfg
 sed -i 's,#command\[check_disk\]=/usr/lib64/nagios/plugins/check_disk \$ARG1\$,commannd\[check_disk\]=/usr/lib64/nagios/plugins/check_disk -w 20% -c 10%,g' /etc/nagios/nrpe.cfg
+#sed -i 's|#command\[check_disk\]=/usr/lib64/nagios/plugins/check_disk \$ARG1\$|commannd\[check_disk\]=/usr/lib64/nagios/plugins/check_disk -w 20% -c 10%|g' /etc/nagios/nrpe.cfg
+
+#sed -i 's|#command\[check_swap\]=/usr/lib64/nagios/plugins/check_swap \$ARG1\$|commannd\[check_swap\]=/usr/lib64/nagios/plugins/check_swap -w 20% -c 10%|g' /etc/nagios/nrpe.cfg
 sed -i 's,#command\[check_swap\]=/usr/lib64/nagios/plugins/check_swap \$ARG1\$,commannd\[check_swap\]=/usr/lib64/nagios/plugins/check_swap -w 20% -c 10%,g' /etc/nagios/nrpe.cfg
-sed -i 's,#command\[check_cpu_stats\]=/usr/lib64/nagios/plugins/check_cpu_stats.sh \$ARG1\$,commannd\[check_cpu_stats\]=/usr/lib64/nagios/plugins/check_cpu_stats.sh -w 70,40,30, -c 90,50,40,g' /etc/nagios/nrpe.cfg
-sed -i 's,#command\[check_mem\]=/usr/lib64/nagios/plugins/check_mem.sh \$ARG1\$,commannd\[check_mem\]=/usr/lib64/nagios/plugins/check_mem.sh -w 80 -c 90,g' /etc/nagios/nrpe.cfg
+
+sed -i 's|#command\[check_cpu_stats\]=/usr/lib64/nagios/plugins/check_cpu_stats.sh \$ARG1\$|commannd\[check_cpu_stats\]=/usr/lib64/nagios/plugins/check_cpu_stats.sh -w 70,40,30, -c 90,50,40|g' /etc/nagios/nrpe.cfg
+
+#sed -i 's|#command\[check_mem\]=/usr/lib64/nagios/plugins/custom_check_mem -n \$ARG1$\|commannd\[check_mem\]=/usr/lib64/nagios/plugins/check_mem.sh -w 80 -c 90|g' /etc/nagios/nrpe.cfg
+sed -i 's,#command\[check_mem\]=/usr/lib64/nagios/plugins/custom_check_mem -n \$ARG1\$,commannd\[check_mem\]=/usr/lib64/nagios/plugins/check_mem.sh -w 80 -c 90,g' /etc/nagios/nrpe.cfg
+
 #restart the nrpe
 systemctl restart nrpe
 
