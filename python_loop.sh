c0182292c123bdeff2d009021710b6388dffacb1
Dara <43659748+DaraUng@users.noreply.github.com>
2019-06-06 18:00:11 -0700

    Create python_loop.sh

---
+++ python_loop.sh
@@ -0,0 +1,15 @@
+current_date = date
+
+Expirydate = ($whois google.com | grep Expiry)
+
+For 
+
+https://github.com/adfinis-sygroup/check-ssl/blob/master/check_ssl.sh
+https://adfinis-sygroup.ch/en/blog/ssl-certificate-monitoring-with-bash/
+https://stackoverflow.com/questions/16703292/bash-script-checking-todays-date-against-a-date-without-year
+https://www.unix.com/unix-for-dummies-questions-and-answers/173323-comparing-output-date-current-system-date.html
+https://unix.stackexchange.com/questions/330774/how-to-compare-two-dates-in-script-to-check-expiration/330802
+https://www.cyberciti.biz/tips/howto-monitor-domain-expiration-renew-date.html
+https://stackoverflow.com/questions/44394034/how-to-view-syslog-in-ubuntu
+https://stackoverflow.com/questions/28226229/bash-looping-through-dates
+https://opensource.com/article/16/12/using-calendar-function-linux
