25a8fbdb28f1fb3cde4a32a4703e17cf02a7ecce
Dara <43659748+DaraUng@users.noreply.github.com>
2019-06-09 09:58:52 -0700

    Update for_loop_rsyslog.sh

--- for_loop_rsyslog.sh
+++ for_loop_rsyslog.sh
@@ -1,3 +1,3 @@
 for servername in $( gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios-install );  do 
-    gcloud compute ssh --zone us-west1-b dara_ung@$servername --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/DaraUng/NTI-320/master/rsyslog.sh?_sm_au_=iVV4SDvqwvsZQMMs && sudo bash rsyslog.sh'
+    gcloud compute ssh --zone us-west1-b dara_ung@$servername --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/DaraUng/NTI-320/master/rsyslog.sh && sudo bash rsyslog.sh'
 done
