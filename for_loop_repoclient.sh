e7bca4a3dd64895541e725c1ea095c544bfa64e2
Dara <43659748+DaraUng@users.noreply.github.com>
2019-06-12 18:49:11 -0700

    Update and rename for_loop_reposrv.sh to for_loop_repoclient.sh

---
+++ for_loop_repoclient.sh
@@ -0,0 +1,3 @@
+for servername in $( gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios-install );  do 
+    gcloud compute ssh --zone us-west1-b dara_ung@$servername --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/DaraUng/NTI-320/master/repoclient.sh && sudo bash repoclient.sh'
+done
