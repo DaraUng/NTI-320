4b7ebf6e9b156ca51dd15ce6b41ae400ed64456a
Dara <43659748+DaraUng@users.noreply.github.com>
2019-06-12 22:43:10 -0700

    Update rsyslog.sh

--- rsyslog.sh
+++ rsyslog.sh
@@ -12,13 +12,13 @@ cp /etc/rsyslog.conf /etc/rsyslog.conf.bak
 
 #Search for the ####### and replace with ##### in the /etc/rsyslog.conf
 
-sed -i 's,#$ModLoad imudp\$,ModLoad imudp,g' /etc/rsyslog.conf
-sed -i 's,#$UDPServerRun 514\$,UDPServerRun 514,g' /etc/rsyslog.conf
-sed -i 's,#$ModLoad imtcp\$,ModLoad imtcp,g' /etc/rsyslog.conf
-sed -i 's,#$InputTCPServerRun 514\$,InputTCPServerRun 514,g' /etc/rsyslog.conf
+sed -i 's/\#$ModLoad imudp,/\$ModLoad imudp,g' /etc/rsyslog.conf
+sed -i 's/\#$UDPServerRun 514,/\$UDPServerRun 514,g' /etc/rsyslog.conf
+sed -i 's/\#$ModLoad imtcp\$ModLoad imtcp,g' /etc/rsyslog.conf
+sed -i 's/\#$InputTCPServerRun 514,/\$InputTCPServerRun 514,g' /etc/rsyslog.conf
 
 #sed -i 's,#command\[check_mem\]=/usr/lib64/nagios/plugins/custom_check_mem -n \$ARG1\$,command\[check_mem\]=/usr/lib64/nagios/plugins/check_mem.sh -w 80 -c 90,g' /etc/nagios/nrpe.cfg
-
+sed -i "s/\$database_username = 'cactiuser';/\$database_username = 'cacti';/" /etc/cacti/db.php
 
 
 systemctl restart rsyslog
