48d21b5ae3ca9635b1c7a5973d6902396b30ca08
Dara <43659748+DaraUng@users.noreply.github.com>
2019-06-13 17:55:42 -0700

    Update rsyslog.sh

--- rsyslog.sh
+++ rsyslog.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 #https://www.tecmint.com/install-rsyslog-centralized-logging-in-centos-ubuntu/
 
-sudo yum -y update && yum -y install rsyslog
+yum -y update && yum -y install rsyslog
 
 systemctl start rsyslog
 systemctl enable rsyslog
@@ -12,13 +12,10 @@ cp /etc/rsyslog.conf /etc/rsyslog.conf.bak
 
 #Search for the ####### and replace with ##### in the /etc/rsyslog.conf
 
-sed -i 's,\#$ModLoad imudp,\$ModLoad imudp,g' /etc/rsyslog.conf
-sed -i 's,\#$UDPServerRun 514,\$UDPServerRun 514,g' /etc/rsyslog.conf
-sed -i 's,\#$ModLoad imtcp,\$ModLoad imtcp,g' /etc/rsyslog.conf
-sed -i 's,\#$InputTCPServerRun 514,\$InputTCPServerRun 514,g' /etc/rsyslog.conf
-
-#sed -i 's,#command\[check_mem\]=/usr/lib64/nagios/plugins/custom_check_mem -n \$ARG1\$,command\[check_mem\]=/usr/lib64/nagios/plugins/check_mem.sh -w 80 -c 90,g' /etc/nagios/nrpe.cfg
-#sed -i "s/\$database_username = 'cactiuser';/\$database_username = 'cacti';/" /etc/cacti/db.php
+sed -i 's/#$ModLoad imudp,/$ModLoad imudp/g' /etc/rsyslog.conf
+sed -i 's/#$UDPServerRun 514/$UDPServerRun 514/g' /etc/rsyslog.conf
+sed -i 's/#$ModLoad imtcp/$ModLoad imtcp/g' /etc/rsyslog.conf
+sed -i 's/#$InputTCPServerRun 514/$InputTCPServerRun 514/g' /etc/rsyslog.conf
 
 
 systemctl restart rsyslog
