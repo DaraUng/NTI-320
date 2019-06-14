0410419e7530666c27cc885dc4a288be96110875
Dara <43659748+DaraUng@users.noreply.github.com>
2019-06-13 18:51:39 -0700

    Update rsyslog.sh

--- rsyslog.sh
+++ rsyslog.sh
@@ -12,7 +12,7 @@ cp /etc/rsyslog.conf /etc/rsyslog.conf.bak
 
 #Search for the ####### and replace with ##### in the /etc/rsyslog.conf
 
-sed -i 's/#\$ModLoad imudp,/\$ModLoad imudp/g' /etc/rsyslog.conf
+sed -i 's/#\$ModLoad imudp/\$ModLoad imudp/g' /etc/rsyslog.conf
 sed -i 's/#\$UDPServerRun 514/\$UDPServerRun 514/g' /etc/rsyslog.conf
 sed -i 's/#\$ModLoad imtcp/\$ModLoad imtcp/g' /etc/rsyslog.conf
 sed -i 's/#\$InputTCPServerRun 514/\$InputTCPServerRun 514/g' /etc/rsyslog.conf
