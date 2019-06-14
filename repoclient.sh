3c30caa1a43599119887fd39de6a155b23a637ca
Dara <43659748+DaraUng@users.noreply.github.com>
2019-06-13 20:11:29 -0700

    Update repoclient.sh

--- repoclient.sh
+++ repoclient.sh
@@ -3,14 +3,14 @@
 
 ip="10.138.0.19"                                                    # make sure the address is a static ip adress.
 
-echo "[nti-320]
+echo '[nti-320]
 name=Extra Packages for Centos from NTI-320 7 - $basearch
 #baseurl=http://download.fedoraproject.org/pub/epel/7/$basearch <- example epel repo
 # Note, this is putting repodata at packages instead of 7 and our path is a hack around that.
 baseurl=http://$ip/centos/7/extras/x86_64/Packages/
 enabled=1
 gpgcheck=0
-" >> /etc/yum.repos.d/NTI-320.repo                                  
+' >> /etc/yum.repos.d/NTI-320.repo                                  
                                                                     # Now that the repo is added, list all repos and make sure
                                                                     # it shows up without error
                                                                     # Use the for_loop_nrpe.sh file to have the reposrv push to all
